3cec8d5ec7a1fcc8d8704974bcfa60a123008cf4
Leonardo Gomide <leocgomide@gmail.com>
2019-03-20 15:46:20 -0300

    Tutorial Parte 5

--- Source/src/ch/makery/address/MainApp.java
+++ Source/src/ch/makery/address/MainApp.java
@@ -9,12 +9,21 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import ch.makery.address.model.Person;
+import ch.makery.address.model.PersonListWrapper;
 import ch.makery.address.view.PersonEditDialogController;
 import ch.makery.address.view.PersonOverviewController;
+import ch.makery.address.view.RootLayoutController;
+import java.io.File;
+import java.util.prefs.Preferences;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 public class MainApp extends Application {
 
@@ -60,21 +69,33 @@ public class MainApp extends Application {
     }
     
     /**
-     * Inicializa o root layout (layout base).
-     */
+    * Inicializa o root layout e tenta carregar o último arquivo
+    * de pessoa aberto.
+    */
     public void initRootLayout() {
         try {
             // Carrega o root layout do arquivo fxml.
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
-            
+
             // Mostra a scene (cena) contendo o root layout.
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
+
+            // Dá ao controller o acesso ao main app.
+            RootLayoutController controller = loader.getController();
+            controller.setMainApp(this);
+
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        // Tenta carregar o último arquivo de pessoa aberto.
+        File file = getPersonFilePath();
+        if (file != null) {
+            loadPersonDataFromFile(file);
         }
     }
 
@@ -107,35 +128,35 @@ public class MainApp extends Application {
     * @param person O objeto pessoa a ser editado
     * @return true Se o usuário clicou OK,  caso contrário false.
     */
-   public boolean showPersonEditDialog(Person person) {
-       try {
-           // Carrega o arquivo fxml e cria um novo stage para a janela popup.
-           FXMLLoader loader = new FXMLLoader();
-           loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
-           AnchorPane page = (AnchorPane) loader.load();
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Carrega o arquivo fxml e cria um novo stage para a janela popup.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
 
-           // Cria o palco dialogStage.
-           Stage dialogStage = new Stage();
-           dialogStage.setTitle("Edit Person");
-           dialogStage.initModality(Modality.WINDOW_MODAL);
-           dialogStage.initOwner(primaryStage);
-           Scene scene = new Scene(page);
-           dialogStage.setScene(scene);
+            // Cria o palco dialogStage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
 
-           // Define a pessoa no controller.
-           PersonEditDialogController controller = loader.getController();
-           controller.setDialogStage(dialogStage);
-           controller.setPerson(person);
+            // Define a pessoa no controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
 
-           // Mostra a janela e espera até o usuário fechar.
-           dialogStage.showAndWait();
+            // Mostra a janela e espera até o usuário fechar.
+            dialogStage.showAndWait();
 
-           return controller.isOkClicked();
-       } catch (IOException e) {
-           e.printStackTrace();
-           return false;
-       }
-   }
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
     
     /**
      * Retorna o palco principal.
@@ -148,4 +169,102 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    /**
+     * Retorna o arquivo de preferências da pessoa, o último arquivo que foi aberto.
+     * As preferências são lidas do registro específico do SO (Sistema Operacional). 
+     * Se tais prefêrencias não puderem  ser encontradas, ele retorna null.
+     * 
+     * @return
+     */
+    public File getPersonFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Define o caminho do arquivo do arquivo carregado atual. O caminho é persistido no
+     * registro específico do SO (Sistema Operacional).
+     * 
+     * @param file O arquivo ou null para remover o caminho
+     */
+    public void setPersonFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp - " + file.getName());
+        } else {
+            prefs.remove("filePath");
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp");
+        }
+    }    
+    
+    /**
+     * Carrega os dados da pessoa do arquivo especificado. A pessoa atual
+     * será substituída.
+     * 
+     * @param file
+     */
+    public void loadPersonDataFromFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            // Reading XML from the file and unmarshalling.
+            PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+            personData.clear();
+            personData.addAll(wrapper.getPersons());
+
+            // Save the file path to the registry.
+            setPersonFilePath(file);
+
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Erro");
+            alert.setHeaderText("Não foi possível carregar dados do arquivo "+file.getPath());
+            alert.setContentText(e.getMessage());
+        }
+    }
+
+    /**
+     * Salva os dados da pessoa atual no arquivo especificado.
+     * 
+     * @param file
+     */
+    public void savePersonDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Envolvendo nossos dados da pessoa.
+            PersonListWrapper wrapper = new PersonListWrapper();
+            wrapper.setPersons(personData);
+
+            // Enpacotando e salvando XML  no arquivo.
+            m.marshal(wrapper, file);
+
+            // Saalva o caminho do arquivo no registro.
+            setPersonFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Erro");
+            alert.setHeaderText("Não foi possível salvar os dados do arquivo "+file.getPath());
+            alert.setContentText(e.getMessage());
+
+            alert.showAndWait();
+        }
+    }    
 }
